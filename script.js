1b02fc7cf6aa86fbde276392a8143013c038a9f0
gato365 <immanuelwilliams@gato365.com>
2022-11-04 06:58:29 -0700

    Ability to see text as it is typed in Console Log

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ var index = 0;
 var trueString = trueStringObject[index].trueString;
 var labeled = trueStringObject[index].labeled;
 var ordered = trueStringObject[index].ordered;
-var enteredString = '27182818284';
-var lengthOfString = enteredString.length;
+var lengthOfString;
 var xR = 0;
 var xW = 0;
 var stringColor;
@@ -11,11 +10,20 @@ var outputString;
 var propCorrect;
 var propWrong;
 var correctSolution = document.querySelector("#correct-solution");
+correctSolution.setAttribute('id', 'correct-solution');
+correctSolution.innerHTML = trueString;
 
-correctSolution .setAttribute('id', 'correct-solution');
-correctSolution .innerHTML = trueString;
+
+var enteredString = document.querySelector("#entered-string");
+// var enteredString = '27182818284';
 
 
+enteredString.addEventListener("keydown", function (event) {
+
+
+    // lengthOfString = enteredString.length;
+    console.log(event.key);
+});
 
 
 for (let i = 0; i <= lengthOfString; i++) {
@@ -24,16 +32,16 @@ for (let i = 0; i <= lengthOfString; i++) {
     if (enteredString[i] == trueString[i]) {
         stringColor = 'black';
         xR++;
-        
+
     } else if (enteredString[i] != trueString[i]) {
         stringColor = 'red';
         xW++;
     }
-    propRight = xR/lengthOfString;
-    propWrong = xW/lengthOfString;
+    propCorrect = xR / lengthOfString;
+    propWrong = xW / lengthOfString;
 
 }
 
-console.log(propRight)
+console.log(propCorrect)
 
 
