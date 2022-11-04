74025d6c7071a7c894a6de77763cb956bf57d44a
gato365 <immanuelwilliams@gato365.com>
2022-11-04 07:11:04 -0700

    Learning to store data as it is entered

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ var lengthOfString;
 var xR = 0;
 var xW = 0;
 var stringColor;
-var outputString;
+
 var propCorrect;
 var propWrong;
+
+var outputString = document.querySelector("#output-string");
+outputString.setAttribute('id', 'output-string');
 var correctSolution = document.querySelector("#correct-solution");
 correctSolution.setAttribute('id', 'correct-solution');
 correctSolution.innerHTML = trueString;
+
 
 
 var enteredString = document.querySelector("#entered-string");
@@ -21,8 +25,12 @@ var enteredString = document.querySelector("#entered-string");
 enteredString.addEventListener("keydown", function (event) {
 
 
-    // lengthOfString = enteredString.length;
-    console.log(event.key);
+    lengthOfString = event.key;
+    
+    // var displayString = '';
+    
+    // outputString.innerHTML = event.key;
+    // console.log(event.key);
 });
 
 
